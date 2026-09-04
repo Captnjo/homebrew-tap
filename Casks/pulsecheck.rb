8f787c3422b518effaf83fa2704706653f1a9a88
captnjo <joseph.k89@gmail.com>
2026-09-04 15:11:59 +0400

    pulsecheck 1.3.4

--- Casks/pulsecheck.rb
+++ Casks/pulsecheck.rb
@@ -1,6 +1,6 @@
 cask "pulsecheck" do
-  version "1.3.3"
-  sha256 "1955b23904bce64ccde6bd810f8290ab44c24ec58410b908b7cfc84e8cc06c43"
+  version "1.3.4"
+  sha256 "673d0903eeca2a4893f768a018ee0f114c57f4ecbc242526a7d05cca685c483a"
 
   url "https://github.com/Captnjo/pulsecheck/releases/download/v#{version}/PulseCheck-#{version}.dmg"
   name "PulseCheck"
